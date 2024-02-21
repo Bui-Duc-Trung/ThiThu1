007d8f10bc72595460c5d832a0e613bb98b66f3f
Admin <Admin@BUIDUCTRUNGG>
2024-02-21 10:48:43 +0700

    Add project files.

---
+++ NhanVien.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThiThu1
+{
+    internal class NhanVien
+    {
+        private int ID { get; set; }
+        private string Ten { get; set; }
+        private double Luong { get; set; }
+        
+        public virtual void InThongTin()
+        {
+            Console.WriteLine($"ID NV: {this.ID} - Ten NV: {this.Ten} - Luong NV: {this.Luong}");
+
+        }
+        public NhanVien()
+        {
+            
+        }
+
+        public NhanVien(int iD, string ten, double luong)
+        {
+            ID = iD;
+            Ten = ten;
+            Luong = luong;
+        }
+    }
+}
---
+++ QLNhanVien.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThiThu1
+{
+    internal class QLNhanVien
+    {
+        public List<NhanVien> DanhSachNV {  get; set; }
+        public QLNhanVien ()
+        {
+            DanhSachNV = new List<NhanVien>();
+        }
+        public void ThemNhanVien()
+        {
+            int tieptuc = -1;
+            do
+            {
+                Console.Write("Nhap ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nhap ten: ");
+                string ten = Console.ReadLine();
+                Console.Write("Nhap luong: ");
+                double luong = double.Parse(Console.ReadLine());
+                NhanVien nhanVien = new NhanVien(id, ten, luong);
+                this.DanhSachNV.Add(nhanVien);
+                Console.Write("Co them NV nua khong (1-co/0-khong): ");
+                tieptuc = int.Parse(Console.ReadLine());
+            } while (tieptuc == 1);
+        }
+        public void XuatDanhSachNhanVien()
+        {
+            foreach (NhanVien nv in this.DanhSachNV)
+            {
+                nv.InThongTin();
+            }
+        }
+    }
+}
